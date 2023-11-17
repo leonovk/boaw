c3661a2fabcaca51d647bcadc117704353b29c0c
Kirill Leonov <leonov835@yandex.ru>
2023-11-17 19:17:34 +0300

    desc

--- lib/boaw.rb
+++ lib/boaw.rb
@@ -8,14 +8,20 @@ class Boaw
     @adapter = Xdotool::Adapter.new
   end
 
+  # method returns the current cursor position
+  # { :x => 1, :y =>2 }
   def position
     adapter.position
   end
 
+  # clicks with the left mouse button,
+  # takes the position coordinates as input
   def left_click(position)
     adapter.left_click(position)
   end
 
+  # clicks with the right mouse button,
+  # takes the position coordinates as input
   def right_click(position)
     adapter.right_click(position)
   end
